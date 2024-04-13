32e7a36763390fa1344840829c2eb089f4cc212a
NTUI RAOUL <134698011+ntuiraoul1@users.noreply.github.com>
2024-04-13 00:46:03 -0400

    Add files via upload

---
+++ ifstatement.py
@@ -0,0 +1,33 @@
+# # If Statement
+
+# a = False
+# b = 3
+
+
+# if a == True:
+#     # print(str(a) + 'is greater than '+str(b))
+#     print('A is true')
+# elif a == False:
+#     print('A is False')
+# else:
+#     print('A is none of the two')
+
+
+# Hereis a program to check if a number 
+# is divisible by 5 or not 
+
+
+# value = int(input('Input a number: '))
+
+# if value % 5  ==0:
+#     print(value, 'can be divided by 5')
+# else:
+#     print(value, 'can not be divided by 5')
+
+
+value = int(input('Input a number: '))
+
+if value % 2  ==0:
+    print(value, ' is an even number')
+else:
+    print(value, ' is an odd number')
